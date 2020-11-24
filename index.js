fdfdbf59fd2a82af589b582a290675afea21127d
JamesERobinson <jameserobinson173@gmail.com>
2020-11-24 15:59:40 +0000

    completed username to my own specs :)

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function validateUserName(event){
-    console.dir(event)
+const userNameError = document.getElementById('signup-username-error')
+
+function validateUserName(event) {
+    const userNameInput = event.target.value;
+
+    const isValid = /\d+/.test(userNameInput)
+    console.log(userNameInput.value)
+    console.log(isValid)
+    if (isValid) {
+        userNameError.innerText = '';
+    } else {
+        userNameError.innerText = "Please include a number in your username"
+    }
 }
+//must be a mix of letters, numbers and at least one special character
